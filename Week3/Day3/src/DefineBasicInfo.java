e07ca2abe8580461479996bb8e137cf0d3b41ad7
Csigi Zsolt <csigizsoltattila@gmail.com>
2020-05-30 14:26:28 +0200

    added other workshop

---
+++ Week3/Day3/src/DefineBasicInfo.java
@@ -0,0 +1,17 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        String name = "Zsolt, Csigi";
+        // Your age as an integer
+        int age = 29;
+        // Your height in meters as a double
+        double height = 1.82;
+        // Whether you are married or not as a boolean
+        boolean married = true;
+        System.out.println("My name is: " + name);
+        System.out.println("My age is: " + age);
+        System.out.println("MY height is: " + height + " meter");
+        System.out.println("Are you married? " + married);
+    }
+}
---
+++ Week3/Day3/src/VariableMutation.java
@@ -0,0 +1,54 @@
+public class VariableMutation {
+    public static void main(String[] args) {
+        int a = 3;
+        // make the "a" variable's value bigger by 10
+
+        System.out.println(a + 10);
+
+        int b = 100;
+        // make b smaller by 7
+
+        System.out.println(b - 7);
+
+        int c = 44;
+        // please double c's value
+
+        System.out.println(c * 2);
+
+        int d = 125;
+        // please divide by 5 d's value
+
+        System.out.println(d / 5);
+
+        int e = 8;
+        // please cube of e's value
+
+        System.out.println(e*e*e);
+
+        int f1 = 123;
+        int f2 = 345;
+        // tell if f1 is bigger than f2 (print as a boolean)
+        boolean fquit = f1 > f2;
+        System.out.println("f1 is bigger than f2? " + fquit );
+
+        int g1 = 350;
+        int g2 = 200;
+        // tell if the double of g2 is bigger than g1 (print as a boolean)
+        boolean gquit = g1 < (g2*2);
+        System.out.println("The double of g2 is bigger than g1? " + gquit );
+
+        int h = 135798745;
+        // tell if it has 11 as a divisor (print as a boolean)
+        boolean hquit = h % 11 == 0;
+        System.out.println("it has 11 as a divisor? " + hquit );
+
+        int i1 = 10;
+        int i2 = 3;
+        // tell if i1 is higher than i2 squared and smaller than i2 cubed (print as a boolean)
+        boolean iquit = i1 > (i2*i2) && i1 < (i2*i2*i2);
+        System.out.println("i1 is higher than i2 squared and smaller than i2 cubed? " + iquit );
+
+        int j = 1521;
+        // tell if j is dividable by 3 or 5 (print as a boolean)
+    }
+}
