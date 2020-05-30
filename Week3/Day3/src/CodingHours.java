e951e55a7f03d39f6de14b846319e9c5b4b6f011
Csigi Zsolt <csigizsoltattila@gmail.com>
2020-05-30 11:57:50 +0200

    added the first 4 project in practice

---
+++ Week3/Day3/src/CodingHours.java
@@ -0,0 +1,17 @@
+public class CodingHours {
+    public static void main(String[] args) {
+        // An average Green Fox attendee codes 6 hours daily
+        // The semester is 17 weeks long
+        //
+        int hoursPerDaily = 6;
+        int weekPerSemester = 17;
+
+        // Print how many hours is spent with coding in a semester by an attendee,
+        // if the attendee only codes on workdays.
+        //
+        System.out.println("One of the average attendee in Green Fox are coding " + (hoursPerDaily * weekPerSemester * 5) + " hours per semester.");
+
+        // Print the percentage of the coding hours in the semester if the average
+        // work hours weekly is 52
+    }
+}
---
+++ Week3/Day3/src/IntruduceYourself.java
@@ -0,0 +1,20 @@
+import java.sql.SQLOutput;
+
+public class IntruduceYourself {
+        public static void main(String[] args) {
+            // Write a program that prints a few details to the terminal window about you
+            // It should print each detail to a new line.
+            String Name = "Zsolt, Csigi";
+            int Age = 29 ;
+            double height = 1.83;
+            //
+            //  Example output:
+            //  John Doe
+            //  31
+            //  1.87
+
+                System.out.println(Name);
+                System.out.println(Age);
+                System.out.println(height);
+        }
+}
